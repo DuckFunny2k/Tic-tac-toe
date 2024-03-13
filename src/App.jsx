ad3d66e36fc4f0e3520dad5a859e150c7175d81c
DuckFunny2k <lamducfpt2004@gmail.com>
2024-03-13 16:06:18 +0700

    CreatingLogo_displayBasic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player1</span>
+          </li>
+          <li>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player2</span>
+          </li>
+          <li>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        game board
+      </div>
+      Log
+    </main>
+  );
 }
 
-export default App
+export default App;
